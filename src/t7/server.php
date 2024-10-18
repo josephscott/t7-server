09a45c77bf1a818fb54ec0afde2579fabf8a26fc
Joseph Scott <joseph@josephscott.org>
2024-10-17 20:44:28 -0600

    Start the fastroute layer

--- src/t7/server.php
+++ src/t7/server.php
@@ -4,5 +4,27 @@ declare( strict_types = 1 );
 namespace T7;
 
 class Server {
+	private array $routes = [];
 
+	public function __construct(
+		array $routes,
+	) {
+		$this->routes = $routes;
+	}
+
+	public function start() : void {
+		$dispatcher = \FastRoute\cachedDispatcher(
+			function ( \FastRoute\RouteCollector $r ) {
+				foreach( $this->routes as $method => $route ) {
+					foreach( $route as $details ) {
+						$r->addRoute( $method, $details['pattern'], $details['handler'] );
+					}
+				}
+			},
+			[
+				'cacheFile' => '/dev/null',
+				'cacheDisabled' => true,
+			]
+		);
+	}
 }
