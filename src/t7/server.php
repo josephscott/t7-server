1bf009aa28eeb163df740c8ac7b0589722ab38da
Joseph Scott <joseph@josephscott.org>
2024-10-19 08:04:59 -0600

    Flush out the router flow more

--- src/t7/server.php
+++ src/t7/server.php
@@ -3,6 +3,12 @@ declare( strict_types = 1 );
 
 namespace T7;
 
+use function header;
+use function rawurldecode;
+use function rtrim;
+use function strpos;
+use function substr;
+
 class Server {
 	private array $routes = [];
 
@@ -15,10 +21,12 @@ class Server {
 	public function start() : void {
 		$dispatcher = \FastRoute\cachedDispatcher(
 			function ( \FastRoute\RouteCollector $r ) {
-				foreach( $this->routes as $method => $route ) {
-					foreach( $route as $details ) {
-						$r->addRoute( $method, $details['pattern'], $details['handler'] );
-					}
+				foreach( $this->routes as $s ) {
+					// Array:
+					// [0] - method
+					// [1] - path/pattern
+					// [2] - handler
+					$r->addRoute( $s[0], $s[1], $s[2] );
 				}
 			},
 			[
@@ -26,5 +34,36 @@ class Server {
 				'cacheDisabled' => true,
 			]
 		);
+
+		$http_method = $_SERVER['REQUEST_METHOD'] ?? '';
+		$uri = $_SERVER['REQUEST_URI'] ?? '';
+		$pos = strpos( $uri, '?' );
+		if ( $pos !== false ) {
+			$uri = substr( $uri, 0, $pos );
+		}
+
+		$uri = rawurldecode( $uri );
+		$route_info = $dispatcher->dispatch( $http_method, $uri );
+
+		// Route info
+		// [0] - status
+		// [1] - handle/allowed methods
+		// [2] - route vars
+		switch ( $route_info[0] ) {
+			case \FastRoute\Dispatcher::NOT_FOUND:
+				// Redirect trailing slashes
+				if ( substr( $uri, -1 ) === '/' ) {
+					header( 'Location: ' . rtrim( $uri, '/' ), true, 301 );
+					exit();
+				}
+				break;
+			case \FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+				$allowed_methods = $route_info[1];
+				break;
+			case \FastRoute\Dispatcher::FOUND:
+				$handler = $route_info[1];
+				$vars = $route_info[2];
+				break;
+		}
 	}
 }
